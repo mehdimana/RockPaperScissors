bbe13a2354351f19dccd7b10ad87b96e8d140595
Mehdi Mana <mehdi.mana@manaitservices.com>
2018-03-20 17:19:41 +0000

    implement ui

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,102 +1,205 @@
-// Import the page's CSS. Webpack will know what to do with it.
-import "../stylesheets/app.css";
+const Web3 = require("web3");
+const Promise = require("bluebird");
+const truffleContract = require("truffle-contract");
+require("file-loader?name=../index.html!../index.html");
+const $ = require("jquery");
+// Not to forget our built contract
+const rockPaperScissorsJson = require("../../build/contracts/RockPaperScissors.json");
 
-// Import libraries we need.
-import { default as Web3} from 'web3';
-import { default as contract } from 'truffle-contract'
+// Supports Mist, and other wallets that provide 'web3'.
+if (typeof web3 !== 'undefined') {
+    // Use the Mist/wallet/Metamask provider.
+    window.web3 = new Web3(web3.currentProvider);
+} else {
+    // Your preferred fallback.
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545')); 
+}
 
-// Import our contract artifacts and turn them into usable abstractions.
-import metacoin_artifacts from '../../build/contracts/MetaCoin.json'
+Promise.promisifyAll(web3.eth, { suffix: "Promise" });
+Promise.promisifyAll(web3.version, { suffix: "Promise" });
 
-// MetaCoin is our usable abstraction, which we'll use through the code below.
-var MetaCoin = contract(metacoin_artifacts);
+const RockPaperScissors = truffleContract(rockPaperScissorsJson);
+RockPaperScissors.setProvider(web3.currentProvider);
 
-// The following code is simple to show off interacting with your contracts.
-// As your needs grow you will likely need to change its form and structure.
-// For application bootstrapping, check out window.addEventListener below.
-var accounts;
-var account;
-
-window.App = {
-  start: function() {
-    var self = this;
-
-    // Bootstrap the MetaCoin abstraction for Use.
-    MetaCoin.setProvider(web3.currentProvider);
-
-    // Get the initial account balance so it can be displayed.
-    web3.eth.getAccounts(function(err, accs) {
-      if (err != null) {
-        alert("There was an error fetching your accounts.");
-        return;
-      }
-
-      if (accs.length == 0) {
-        alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
-        return;
-      }
-
-      accounts = accs;
-      account = accounts[0];
-
-      self.refreshBalance();
-    });
-  },
-
-  setStatus: function(message) {
-    var status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-
-  refreshBalance: function() {
-    var self = this;
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account, {from: account});
-    }).then(function(value) {
-      var balance_element = document.getElementById("balance");
-      balance_element.innerHTML = value.valueOf();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error getting balance; see log.");
-    });
-  },
-
-  sendCoin: function() {
-    var self = this;
-
-    var amount = parseInt(document.getElementById("amount").value);
-    var receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    var meta;
-    MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.sendCoin(receiver, amount, {from: account});
-    }).then(function() {
-      self.setStatus("Transaction complete!");
-      self.refreshBalance();
-    }).catch(function(e) {
-      console.log(e);
-      self.setStatus("Error sending coin; see log.");
-    });
-  }
-};
+var latestGameHash;
 
 window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
-  }
+    return web3.eth.getAccountsPromise()
+        .then(accounts => {
+            if (accounts.length == 0) {
+                $("#balance").html("N/A");
+                throw new Error("No account with which to transact");
+            }
+            window.account = accounts[0];
 
-  App.start();
+            //update account dropdown
+            $("#account-select").empty();
+            for(i=0; i<accounts.length; i++) {
+                $("#account-select").append($("<option></option>")
+                                   .attr("value", accounts[i]).text(i + ": " + accounts[i]));
+                $("#second-player-select").append($("<option></option>")
+                                   .attr("value", accounts[i]).text(i + ": " + accounts[i]));
+            }
+            $("#account-select").change(function(){
+                updateBalance($(this).val());
+            });
+            // console.log("Account:", window.account);
+            return web3.version.getNetworkPromise();
+        })
+        .then(network => console.log("Network:", network.toString(10)))
+        .then(() => watchEvents())
+        .then(() => updateBalance($("#account-select").val()))
+        .then(() => $("#create-game").click(createGame))
+        .then(() => $("#play-buttont").click(playGame))
+        .then(() => $("#reveal-buttont").click(reveal))
+        .then(() => $("#claim-winner-buttont").click(claimAsWinner))
+        .then(() => $("#claim-draw-buttont").click(claimDraw))
+        // Never let an error go unlogged.
+        .catch(console.error);
 });
+
+const updateBalance = function(accountSelected) {
+  return web3.eth.getBalancePromise(accountSelected)
+                 .then(balance => {
+                    $("#balance").html(balance.toString(10))
+                 }).catch(console.error);
+};
+
+const watchEvents = function() {
+  var deployed;
+  return RockPaperScissors.deployed().then( deploy => {
+          deployed = deploy;
+            return  deploy.LogClaim({from: account});
+         }).then( event => {
+            watchEvent(event);
+            return deployed.LogPlay({from: account});
+         }).then( event => {
+            watchEvent(event);
+            return deployed.LogRevealed({from: account});
+         }).then( event => {
+            watchEvent(event);
+            return deployed.LogGameCreated({from: account});
+         }).then( event => {
+            watchEvent(event);
+         });
+}
+
+const watchEvent = function(event) {
+   event.watch(function(err, result) {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    console.log("Event received: " + result.event);
+    console.log(result.args);
+    if (result.event == "LogGameCreated") {
+      $("#gameHash").html(result.args.gameHash);
+      latestGameHash = result.args.gameHash;
+    }
+  })
+}
+
+const createGame = function() {
+  //console.log("in create game");
+  return RockPaperScissors.deployed().then( deploy => {
+            return  deploy.createGame.sendTransaction($("input[name='gnumber']").val(), 
+                                                      $("#account-select").val(), 
+                                                      $("#second-player-select").val(),
+                                                      $("input[name='stake']").val(),
+                                                      {from: $("#account-select").val(),
+                                                       gas: 5000000});
+        }).then(txHash => {
+          return web3.eth.getTransactionReceiptPromise(txHash);
+        }).then(txObject => {
+          if (txObject.status == "0x01" || txObject.status == 1) {
+            $("#status").html("game created successsfuly.");
+          } else {
+            $("#status").html("error creating game.");
+            console.error(txObject);
+          }
+         }).catch(console.error);
+}
+
+const playGame = function() {
+  //console.log("in play game");
+  var deployed;
+   return RockPaperScissors.deployed().then( deploy => {
+            deployed = deploy;
+            return getMoveHash();
+        }).then(hash => {
+            return  deployed.play.sendTransaction(latestGameHash, 
+                                                  hash, 
+                                                 {from: $("#account-select").val(),
+                                                  value: $("input[name='stake']").val()});
+        }).then(txHash => {
+          return web3.eth.getTransactionReceiptPromise(txHash);
+        }).then(txObject => {
+          if (txObject.status == "0x01" || txObject.status == 1) {
+            $("#status").html("Play successfull.");
+          } else {
+            $("#status").html("Error occured while playing.");
+            console.error(txObject);
+          }
+        }).catch(console.error);
+}
+
+const getMoveHash = function() {
+  //console.log("in getMoveHash");
+   return RockPaperScissors.deployed() .then( deploy => {
+            return  deploy.calculateMovesHash(latestGameHash, 
+                                              $("#account-select").val(),
+                                              $("input[name='pwd']").val(),
+                                              $("#move-select").val(),
+                                              {from: $("#account-select").val()});
+    });
+}
+
+const reveal = function() {
+   return RockPaperScissors.deployed().then( deploy => {
+            return  deploy.reveal.sendTransaction(latestGameHash,
+                                                  $("input[name='pwd']").val(), 
+                                                  $("#move-select").val(),
+                                                  {from: $("#account-select").val()});
+  }).then(txHash => {
+    return web3.eth.getTransactionReceiptPromise(txHash);
+  }).then(txObject => {
+    if (txObject.status == "0x01" || txObject.status == 1) {
+      $("#status").html("move revealed successfuly.");
+    } else {
+      $("#status").html("error revealing move.");
+      console.error(txObject);
+    }
+  }).catch(console.error);
+}
+
+const claimDraw = function() {
+   return RockPaperScissors.deployed().then( deploy => {
+            return  deploy.claimDraw.sendTransaction(latestGameHash,
+                                                    {from: $("#account-select").val()});
+  }).then(txHash => {
+    return web3.eth.getTransactionReceiptPromise(txHash);
+  }).then(txObject => {
+    if (txObject.status == "0x01" || txObject.status == 1) {
+      $("#status").html("claimed successfuly.");
+    } else {
+      $("#status").html("error claiming.");
+      console.error(txObject);
+    }
+  }).catch(console.error);
+}
+
+const claimAsWinner = function() {
+   return RockPaperScissors.deployed().then( deploy => {
+            return  deploy.claimAsWinner.sendTransaction(latestGameHash,
+                                                        {from: $("#account-select").val()});
+  }).then(txHash => {
+    return web3.eth.getTransactionReceiptPromise(txHash);
+  }).then(txObject => {
+    if (txObject.status == "0x01" || txObject.status == 1) {
+      $("#status").html("claimed successsfuly.");
+    } else {
+      $("#status").html("error claiming.");
+      console.error(txObject);
+    }
+  }).catch(console.error);
+}
